e8464d62e3b1fe64e8d3df6d59015bb72f82c318
crcrpar <masakikozuki@Masaki-no-MacBook-Pro.local>
2016-10-20 13:34:55 +0900

    add update_core

--- train_ebgan.py
+++ train_ebgan.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import os
+import six
 import numpy as np
 
 import chainer
@@ -13,6 +14,7 @@ from chainer.training import trainer, extension
 from chainer.dataset import convert
 from chainer.dataset import iterator as iterator_module
 from chainer import optimizer as optimizer_module
+from chaienr import reporter
 
 import net
 
@@ -38,7 +40,7 @@ def pt_regularizer(S, bs=None):
 
 class EBGAN_Updater(chainer.training.StandardUpdater):
 
-    def __init__(self, iterator, generator, discriminator, optimizers, converter=convert.concat_examples, device=None):
+    def __init__(self, iterator, generator, discriminator, optimizers, coeff=0.1 converter=convert.concat_examples, device=None):
         if isinstance(iterator, iterator_module.Iterator):
             iterator = {'main':iterator}
 
@@ -47,7 +49,7 @@ class EBGAN_Updater(chainer.training.StandardUpdater):
 
         self.gen = generator
         self.dis = discriminator
-
+        self._c = coeff
         self.converter = converter
         self.iteration = 0
         self.device = device
@@ -56,7 +58,20 @@ class EBGAN_Updater(chainer.training.StandardUpdater):
         batch = self._iterators['main'].next()
         in_arrays = self.converter(batch, self.device)
         fake_image = self.gan()
-        _data = F.concat((chainer.Variable(in_arrays), fake_image))
-        prediction = self.dis(_data)
 
-        mse_dis = F.mean_squared_error(_data, prediction)
+        reconstructed_true = self.dis(chainer.Variable(in_arrays))
+        reconstructed_false = self.dis(fake_image)
+        loss_gen = F.sqrt(F.sum(F.batch_l2_norm_squared(reconstructed_false - fake_image)))
+
+        loss_dis = F.sqrt(F.sum(F.batch_l2_norm_squared(reconstructed_true - chainer.Variable(in_arrays)))) + F.sqrt(F.sum(F.batch_l2_norm_squared(reconstructed_false - fake_image)))
+
+        reporter.report({'dis/loss': loss_dis, 'gen/loss': loss_gen})
+
+        loss_dictionary = {'dis':loss_dis, 'gen':loss_gen}
+
+        for name, optimizer in six.iteritems(self._optimizers):
+            optimizer.target.cleargrads()
+            loss_dictionary[name].backward()
+            optimizer.update()
+
+            
