9fd341198251bf3b007b146289dedf9747ff22a5
crcrpar <masakikozuki@Masaki-no-MacBook-Pro.local>
2016-10-26 08:35:39 +0900

    modify definition

--- net.py
+++ net.py
@@ -31,20 +31,21 @@ class Discriminator1(chainer.Chain):
         )
 
     def __call__(self, x):
-        h1 = F.relu(self.norm1(self.d1(x)))
-        h2 = F.relu(self.norm2(self.d2(h1)))
-        h3 = F.relu(self.fc3(h2))
-        h4 = F.relu(self.fc4(h3))
+        h1 = F.leaky_relu(self.norm1(self.d1(x)))
+        h2 = F.leaky_relu(self.norm2(self.d2(h1)))
+        h3 = F.leaky_relu(self.fc3(h2))
+        h4 = F.leaky_relu(self.fc4(h3))
         h4 = F.reshape(h4, (-1, 32, 3, 3))
-        h5 = F.relu(self.norm5(self.d5(h4)))
+        h5 = F.leaky_relu(self.norm5(self.d5(h4)))
         h6 = F.sigmoid(self.d6(h5))
+        h6 = self.d6(h5)
 
         return h6
 
     def encode(self, x):
-        h1 = F.relu(self.norm1(self.d1(x)))
-        h2 = F.relu(self.norm2(self.d2(h1)))
-        S = F.relu(self.fc3(h2))
+        h1 = F.leaky_relu(self.norm1(self.d1(x)))
+        h2 = F.leaky_relu(self.norm2(self.d2(h1)))
+        S = F.leaky_relu(self.fc3(h2))
 
         return S
 
@@ -63,8 +64,8 @@ class Generator(chainer.Chain):
         self.z_dim = z_dim
         self.batch_size = batch_size
 
-    def __call__(self, train=True):
-        z = np.random.uniform(size=(self.batch_size, self.z_dim)).astype(np.float32)
+    def __call__(self, bs=self.batch_size, train=True):
+        z = np.random.uniform(size=(bs, self.z_dim)).astype(np.float32)
         if not train:
             z = chainer.Variable(z, volatile='on')
         h1_ = self.fc1(z)
